7405be8cfcee86a5bb488c364000c3b840a1ec1f
HighX97 <loic.ortole@gmail.com>
2016-01-18 11:14:54 +0100

    Init

---
+++ src/helloWorld/Client.java
@@ -0,0 +1,24 @@
+package helloWorld;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+
+public class Client {
+	
+	public Client(){}
+	
+	public static void main(String args[]){
+		String host = (args.length < 1) ? null : args[0];
+		try {
+			Registry registry = LocateRegistry.getRegistry(host);
+			Hello stub =  (Hello) registry.lookup("Hello");
+			String response = stub.sayHello();
+			System.out.println("Response: "+response);
+			stub.printHello();
+		} catch (Exception e)
+		{
+			System.err.println("Client exception "+e.toString());
+			e.printStackTrace();
+		}
+	}
+}
